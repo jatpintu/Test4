4247d4099e73fe324cdd72ec86adf24be8377440
Pintu jat <pintu.chaudhary9@gmail.com>
2019-10-17 10:31:05 -0400

    Initial Commit

---
+++ BabaIsYou-F19/GameScene.swift
@@ -0,0 +1,34 @@
+//
+//  GameScene.swift
+//  BabaIsYou-F19
+//
+//  Created by Parrot on 2019-10-17.
+//  Copyright © 2019 Parrot. All rights reserved.
+//
+
+import SpriteKit
+import GameplayKit
+
+class GameScene: SKScene, SKPhysicsContactDelegate {
+
+    override func didMove(to view: SKView) {
+        self.physicsWorld.contactDelegate = self
+    }
+   
+    func didBegin(_ contact: SKPhysicsContact) {
+        print("Something collided!")
+    }
+    
+    override func update(_ currentTime: TimeInterval) {
+        // Called before each frame is rendered
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+    }
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+    }
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+    }
+}
